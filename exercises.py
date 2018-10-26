f3f9e8bba80de34bb7d83a01e1e6918a14bd336e
Pepe Garcia <pepe@pepegar.com>
2018-10-26 14:54:50 +0200

    create hello function

--- exercises.py
+++ exercises.py
@@ -5,10 +5,15 @@ Created on Fri Oct 26 14:35:09 2018
 
 @author: pepegar
 """
-
+#%%
 def linear(lst, num):
     for i in range(len(lst)):
         if lst[i] == num:
             return i
         
     return None
+
+#%%
+def hello():
+    name  = input("What's your name? ")
+    print("hello " + name)
